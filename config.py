f335e7aadc3dc8ed02dfc37f4a3ac1e39b601ae6
Jayalakshmi Mohan <jaya.vj1989@gmail.com>
2021-12-13 12:40:44 -0600

    big query function 1

--- config.py
+++
@@ -1,6 +0,0 @@
-pgsql_config = {
-     'host': 'ls-41d379b19b475ed294babb170cfa0f93b3011e47.cq2f1e9koedo.us-east-2.rds.amazonaws.com',
-     'dbname': 'jayavj1989',
-     'user': 'dbmasteruser',
-     'password': 'Swnp3XQFtBd)b61NGn!uh{Lw=8#Vk~y<'
- }
